55a962d8c92fbe23489b6a1aa52a2df695280fde
Teddy Owen <david.e.owen1@gmail.com>
2018-08-23 14:12:39 -0400

    tutorial finished

---
+++ app/src/main/java/com/example/teddy/myfirstapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.teddy.myfirstapp;
+
+import android.content.Intent;
+import android.graphics.Camera;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
